69449992df5aaa71786b41c2781d0b2ff32efbc8
Ivy-aminga <amingaivy@gmail.com>
2023-03-09 08:59:14 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,47 @@
+fun main() {
+take("Student","Adalab")
+println(mates.contentToString())
+    println(nos.average())
+    println(nos.min())
+    println(nos.max())
+}
+
+
+//1. Write and invoke a function that takes in any 2 strings and returns the first
+//  character of the longer string or the first character of the first string if they are
+//          equal in length.
+
+fun take (name1: String,name2: String){
+   name1.length
+    name2.length
+    println(name1.length)
+}
+
+
+//2. Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements.
+
+var nos = arrayOf(100,20,40,70,)
+
+
+//3. Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output.
+
+
+//4. Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it.
+
+var mates = arrayOf("Nyeliep","Mumo","Aniso","Ctynthia")
+
+
+
+//5. Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
+//Instantiate your calculator object and invoke all its functions
+
+val digits = arrayOf(100,50)
+
+
